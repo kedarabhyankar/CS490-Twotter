8e9e9f0a85c11c345a1c9565180c9e3fea4577da
Kedar Abhyankar <krabhyankar@gmail.com>
2020-02-09 19:38:16 -0500

    Initial Commit + Login Screen Implemented. Changed Auth to use my authorization so that I could run it on my iPhone 11 Pro.

---
+++ Twitter/LoginViewController.swift
@@ -0,0 +1,39 @@
+//
+//  LoginViewController.swift
+//  Twitter
+//
+//  Created by Kedar Abhyankar on 2/9/20.
+//  Copyright © 2020 Dan. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func onLoginButton(_ sender: Any) {
+        let loginURL = "https://api.twitter.com/oauth/request_token";
+        TwitterAPICaller.client?.login(url: loginURL, success: {
+            self.performSegue(withIdentifier: "loginToHome", sender: self)
+        }, failure: { (Error) in
+            print("Could not log in!")
+        })
+    }
+
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+}
