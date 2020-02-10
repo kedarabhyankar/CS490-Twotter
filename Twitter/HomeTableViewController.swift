6ee9cdfba7324ce016a7d35fd472afd47009ee66
Kedar Abhyankar <krabhyankar@gmail.com>
2020-02-09 21:15:57 -0500

    Login Screen complete, Logout complete, UserDefaults set up to track login logout

---
+++ Twitter/HomeTableViewController.swift
@@ -0,0 +1,32 @@
+//
+//  HomeTableViewController.swift
+//  Twitter
+//
+//  Created by Kedar Abhyankar on 2/9/20.
+//  Copyright © 2020 Dan. All rights reserved.
+//
+
+import UIKit
+
+class HomeTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func onLogout(_ sender: Any) {
+        TwitterAPICaller.client?.logout()
+        self.dismiss(animated: true, completion: nil)
+        UserDefaults.standard.set(false, forKey: "userLoggedIn")
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+}
--- Twitter/LoginViewController.swift
+++ Twitter/LoginViewController.swift
@@ -18,9 +18,17 @@ class LoginViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    override func viewDidAppear(_ animated: Bool){
+        if(UserDefaults.standard.bool(forKey: "userLoggedIn")){
+            UserDefaults.standard.set(true, forKey: "userLoggedIn")
+            self.performSegue(withIdentifier: "loginToHome", sender: self)
+        }
+    }
+    
     @IBAction func onLoginButton(_ sender: Any) {
         let loginURL = "https://api.twitter.com/oauth/request_token";
         TwitterAPICaller.client?.login(url: loginURL, success: {
+            UserDefaults.standard.set(true, forKey: "userLoggedIn")
             self.performSegue(withIdentifier: "loginToHome", sender: self)
         }, failure: { (Error) in
             print("Could not log in!")
