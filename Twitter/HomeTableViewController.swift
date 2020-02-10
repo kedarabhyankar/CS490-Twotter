56ce5a5fed8ec96cddef907ad3f1d5180f058912
Kedar Abhyankar <krabhyankar@gmail.com>
2020-02-10 11:06:47 -0500

    Added Infinite Scroll Feature

--- Twitter/HomeTableViewController.swift
+++ Twitter/HomeTableViewController.swift
@@ -17,14 +17,34 @@ class HomeTableViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        loadTweet()
-        myRefreshControl.addTarget(self, action: #selector(loadTweet), for: .valueChanged)
+        loadTweets()
+        myRefreshControl.addTarget(self, action: #selector(loadTweets), for: .valueChanged)
         tableView.refreshControl = myRefreshControl
     }
     
-    @objc func loadTweet(){
+    @objc func loadTweets(){
+        numberOfTweets = 20
         let apiURL = "https://api.twitter.com/1.1/statuses/home_timeline.json"
-        let params = ["count":20]
+        let params = ["count":numberOfTweets]
+        TwitterAPICaller.client?.getDictionariesRequest(url: apiURL, parameters: params, success: {(tweets:[NSDictionary]) in
+            
+            self.tweetLibrary.removeAll()
+            for tweet in tweets {
+                self.tweetLibrary.append(tweet)
+            }
+            
+            self.tableView.reloadData()
+            self.myRefreshControl.endRefreshing()
+            
+        }, failure: {(Error) in
+            print("Could not retrieve tweet!")
+        })
+    }
+    
+    func loadMoreTweets(){
+        let apiURL = "https://api.twitter.com/1.1/statuses/home_timeline.json"
+        numberOfTweets = numberOfTweets + 20
+        let params = ["count":numberOfTweets]
         TwitterAPICaller.client?.getDictionariesRequest(url: apiURL, parameters: params, success: {(tweets:[NSDictionary]) in
             
             self.tweetLibrary.removeAll()
@@ -53,7 +73,7 @@ class HomeTableViewController: UITableViewController {
         let user = tweetLibrary[indexPath.row]["user"] as! NSDictionary
         cell.userName.text = user["name"] as? String;
         cell.tweetContent.text = tweetLibrary[indexPath.row]["text"] as? String
-       
+        
         let imageURL = URL(string: (user["profile_image_url_https"] as? String)!)
         let data = try? Data(contentsOf: imageURL!)
         
@@ -63,6 +83,12 @@ class HomeTableViewController: UITableViewController {
         
         
         return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath){
+        if(indexPath.row + 1 == tweetLibrary.count){
+            loadMoreTweets()
+        }
     }
     
     override func numberOfSections(in tableView: UITableView) -> Int {
